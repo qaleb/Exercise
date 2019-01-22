82edc21c60b0afb62cf5efd3caadf80de9fb295b
Qaleb <cawam40@gmail.com>
2019-01-22 15:17:06 +0300

    Two problems tackled

--- Sum_of _Squares.py
+++ Sum_of _Squares.py
@@ -1,7 +1,12 @@
-number = 1
+'''1. Write a program which uses a while loop to sum the squares of integers (starting from 1) until the total exceeds
+200. Print the final total and the last number to be squared and added.'''
+
+number = 0
 total = 0
+
 while total < 200:
-    total += (number*number) #adds square of the present integer to the total
     number += 1
-print('Total sum of the squares is: ',total)
-print('The final integer is: ', number)
+    total += number ** 2 #adds square of the present integer to the total
+    
+print('Total sum of the squares is:',total)
+print('The final integer is:', number)
